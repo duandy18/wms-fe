faa3b12976f5c2cf780b2cee4f599f2f2a0f081a
duandy18 <hongshidu888@gmail.com>
2026-03-30 20:50:12 +0800

    chore(repo): remove accidentally committed frontend backup files (#96)
    Co-authored-by: hongshi <hongshi@users.noreply.github.com>

--- .backup/oms-round2-20260329-185846/JdOrdersPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const JdOrdersPage: React.FC = () => {
-  return <div className="p-4">京东 / 订单台账</div>;
-};
-
-export default JdOrdersPage;
--- .backup/oms-round2-20260329-185846/TaobaoOrdersPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TaobaoOrdersPage: React.FC = () => {
-  return <div className="p-4">淘宝 / 订单台账</div>;
-};
-
-export default TaobaoOrdersPage;
